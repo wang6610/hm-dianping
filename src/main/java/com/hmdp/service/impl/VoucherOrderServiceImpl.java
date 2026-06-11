cf59ee7e72b2157c11bd6c9e4a708c1d49610fba
John Doe <1929897318@qq.com>
2026-06-11 15:35:38 +0800

    分布式锁的实现代码

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -8,8 +8,12 @@ import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +35,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private ISeckillVoucherService seckillVoucherService;
     @Resource
     private RedisIdWorker redisIdWorker;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private RedissonClient redissonClient;
 
 
     @Override
@@ -44,7 +52,6 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
         // 3.判断秒杀是否已经结束
         if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
-            // 尚未开始
             return Result.fail("秒杀已经结束！");
         }
         // 4.判断库存是否充足
@@ -54,11 +61,24 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
         Long userId = UserHolder.getUser().getId();
-        synchronized (userId.toString().intern()) {
+        // 创建锁对象
+        // SimpleRedisLock lock = new SimpleRedisLock("order:" + userId,stringRedisTemplate);
+        RLock lock = redissonClient.getLock("lock:order:" + userId);
+        // 获取锁对象
+        boolean isLock = lock.tryLock();
+        // 加锁失败
+        if (!isLock){
+            return Result.fail("不允许重复下单！");
+        }
+        try {
             // 获取代理对象（事务）
             IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
             return proxy.createVoucherOrder(voucherId);
+        } finally {
+            // 释放锁
+            lock.unlock();
         }
+
     }
 
     @Transactional
